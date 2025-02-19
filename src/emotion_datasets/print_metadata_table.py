2707431ad831a7448a34497367e428a5e28071b2
ioverho <i.o.verhoeven@uva.nl>
2025-02-19 11:51:05 +0100

    added isear

--- src/emotion_datasets/print_metadata_table.py
+++ src/emotion_datasets/print_metadata_table.py
@@ -11,6 +11,7 @@ DATASET_SIZES = {
     "EmoInt": "6.9k",
     "FBValenceArousal": "2.9k",
     "GoEmotions": "58k",
+    "ISEAR": "7.6k",
     "REN20k": "20k",
     "SentimentalLIAR": "13k",
     "SSEC": "4.8k",
